5ba86f588e2314986a63b2340d0df6f85d08724d
SamVsCode <samvscode@gmail.com>
2017-09-29 15:51:46 +0530

    Collision detection working, rotation working, merging working

--- js/main.js
+++ js/main.js
@@ -47,31 +47,33 @@ var Blocks = (function() {
 var MatrixMap = (function() {
     var blockKeys = [];
     for (var key in Blocks) {
-        blockKeys.push(key);
+        if (key !== "colors")
+            blockKeys.push(key);
     }
-    function getRandomNumber(min,max)
-	{
-	    return Math.floor(Math.random()*(max-min+1)+min);
-	}
+
+    function getRandomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
     function randomBlockGenerator() {
-    	var b = blockKeys[getRandomNumber(0,blockKeys.length-1)];
-    	console.log(b);
+        var b = blockKeys[getRandomNumber(0, blockKeys.length - 1)];
         return b;
     }
-    function mapCollision(){
-    	var m = Player.brick;
-    	var arena = Window.game.arena;
-    	var o = Player.pos;
-    	for (let y = 0; y < m.length; ++y) {
-        for (let x = 0; x < m[y].length; ++x) {
-            if (m[y][x] !== 0 &&
-               (arena[y + o.y] &&
-                arena[y + o.y][x + o.x]) !== 0) {
-                return true;
+
+    function mapCollision() {
+        var m = Player.brick;
+        var arena = Window.game.arena;
+        var o = Player.pos;
+        for (var y = 0; y < m.length; ++y) {
+            for (var x = 0; x < m[y].length; ++x) {
+                if (m[y][x] !== 0 &&
+                    (arena[y + o.y] &&
+                        arena[y + o.y][x + o.x]) !== 0) {
+                    return true;
+                }
             }
         }
-    }
-    	return false;
+        return false;
     }
 
     function createMatrix(w, h) {
@@ -82,9 +84,40 @@ var MatrixMap = (function() {
         return matrix;
     }
 
-    function rotateMatrix() {}
+    function swap(x1, y1, x2, y2, m) {
+        var temp = m[x1][y1];
+        m[x1][y1] = m[x2][y2];
+        m[x2][y2] = temp;
+    }
 
-    function mergeMatrix() {}
+    function rotateMatrix() {
+        var matrixSize = Window.game.Player.brick.length;
+        var m = Window.game.Player.brick;
+        var count = 0;
+        var level = 0;
+        var last = matrixSize - 1;
+        var totalLevels = matrixSize / 2;
+
+        while (level < totalLevels) {
+            for (var i = level; i < last; i++) {
+                swap(level, i, i, last, m);
+                swap(level, i, last, last - i + level, m);
+                swap(level, i, last - i + level, level, m);
+            }
+            ++level;
+            --last;
+        }
+    }
+
+    function mergeMatrix() {
+        Window.game.Player.brick.forEach((row, y) => {
+            row.forEach((value, x) => {
+                if (value !== 0) {
+                    Window.game.arena[y + Window.game.Player.pos.y][x + Window.game.Player.pos.x] = value;
+                }
+            });
+        });
+    }
 
     function drawMatrix(matrix, offset, ctx) {
         for (var j = 0; j < matrix.length; j++) {
@@ -99,8 +132,8 @@ var MatrixMap = (function() {
     }
 
     return {
-    	getRandomNumber: getRandomNumber,
-    	randomBlockGenerator: randomBlockGenerator,
+        getRandomNumber: getRandomNumber,
+        randomBlockGenerator: randomBlockGenerator,
         createMatrix: createMatrix,
         rotateMatrix: rotateMatrix,
         mergeMatrix: mergeMatrix,
@@ -110,22 +143,28 @@ var MatrixMap = (function() {
 }());
 
 var Player = (function() {
-	var pos = { x: 0, y: 0 };
-	var brick = Blocks[MatrixMap.randomBlockGenerator()];
+    var pos = {
+        x: 0,
+        y: 0
+    };
+    var brick = Blocks[MatrixMap.randomBlockGenerator()];
+
     function drop() {
         Player.pos.y++;
-        console.log(MatrixMap.mapCollision());
-        if(MatrixMap.mapCollision()){
-        	Player.pos.y--;
-        	Player.brick = nextBrick();
+        if (MatrixMap.mapCollision()) {
+            Player.pos.y--;
+            MatrixMap.mergeMatrix();
+            Player.brick = nextBrick();
         }
     }
-    function move(i){
-    	Window.game.Player.pos.x+=i;
+
+    function move(i) {
+        Window.game.Player.pos.x += i;
     }
-    function nextBrick(){
-    	Player.pos.y = 0;
-    	return Blocks[MatrixMap.randomBlockGenerator()];
+
+    function nextBrick() {
+        Player.pos.y = 0;
+        return Blocks[MatrixMap.randomBlockGenerator()];
     }
     return {
         drop: drop,
@@ -134,7 +173,7 @@ var Player = (function() {
         score: 0,
         nextBrick: nextBrick,
         move: move,
-        brick:brick
+        brick: brick
     };
 }());
 
@@ -158,14 +197,13 @@ var Game = (function() {
         }
 
         this.configuration = {
-            speed: 1,
+            speed: 0.3,
             gravity: 0.30,
             score: 0,
             grid: 32
         };
         this.Player = Player;
-        this.arena = MatrixMap.createMatrix(Math.floor(this.canvas.width/this.configuration.grid),Math.floor(this.canvas.height/this.configuration.grid));
-        console.log(this.arena);
+        this.arena = MatrixMap.createMatrix(Math.floor(this.canvas.width / this.configuration.grid), Math.floor(this.canvas.height / this.configuration.grid));
     }
 
     Game.prototype.draw = function() {
@@ -177,14 +215,23 @@ var Game = (function() {
             Player.drop();
             this.dropCounter = 0;
         }
+        MatrixMap.drawMatrix(this.arena, { x: 0, y: 0 }, this.ctx);
     }
     Game.prototype.keyPressed = function(key) {
-    	if(key===37){
-    		Window.game.Player.move(-1);
-    	}
-    	if(key===39){
-    		Window.game.Player.move(1);
-    	}
+        if (key === 37) {
+            Window.game.Player.move(-1);
+        }
+        if (key === 39) {
+            Window.game.Player.move(1);
+        }
+        if (key === 38) {
+            MatrixMap.rotateMatrix();
+        }
+        // if (key === 40) {
+        //     console.log(Window.game.Player.pos.y, Window.game.arena.length);
+        //     if (Window.game.Player.pos.y < Window.game.arena.length - 3 && !MatrixMap.mapCollision())
+        //         Window.game.Player.pos.y++;
+        // }
     }
     Game.prototype.keyReleased = function(key) {}
 
